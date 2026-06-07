cf7bfdc4c159000ed6a1e5c8ba5caf255b8e4694
gi8 <giottolino@gmail.com>
2026-06-07 13:54:27 +0200

    chore(tests): remove split-suite placeholder files

--- Tests/EasyBarTests/ConfigLoaderTests.swift
+++
@@ -1,2 +0,0 @@
-// Config loader tests were split by behavior.
-// See ConfigLoaderPathTests.swift, ConfigLoaderThemeTests.swift, ConfigLoaderLoggingTests.swift, ConfigLoaderLuaCommandTests.swift, and ConfigLoaderBuiltinTests.swift.
--- Tests/EasyBarTests/LuaRenderCoalescingTests.swift
+++
@@ -1,2 +0,0 @@
-// Lua render tests were split by behavior.
-// See LuaRenderCommandTests.swift, LuaRenderStateTests.swift, and LuaRenderIntervalTests.swift.
